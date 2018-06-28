c084c82164bc11430e9196d532004922ed105e90
ma539@cornell.edu <ma539@cornell.edu>
2018-06-28 22:06:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each{|x| new_array << x**2}
+  return new_array
 end
+
+# Advanced:
+# def square_array(array)
+#   array.collect {|x| x**2}
+# end
